21d19d3730e7c2a22f49d0bc840c7f850906203e
miss-gif <ddc.akari@gmail.com>
2024-04-29 22:40:14 +0900

    init 초기설정

---
+++ README.md
@@ -0,0 +1,13 @@
+# 프로젝트 생성
+
+`npm init vite ./`
+`npm i`
+
+## 라이브러리 추가 설치
+
+axios
+react-router-dom
+autoprefixer
+postcss
+tailwind-scrollbar
+tailwindcss
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+        obcaecati totam eos aliquid quos asperiores itaque, pariatur natus?
+        Ducimus reprehenderit maxime consequuntur quas omnis deleniti
+        praesentium ab ad veritatis quaerat.
+      </h1>
+    </div>
+  );
+};
+
+export default App;
