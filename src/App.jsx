c99bb87415230ef5a06a09bc27a458fdeb14ea84
miss-gif <ddc.akari@gmail.com>
2024-04-29 23:21:59 +0900

    Test : 포켓몬 API 출력

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,33 @@ const App = () => {
   const fetchPokeDate = async () => {
     try {
       const response = await axios.get(url);
-      console.log(response.data.results);
+      // console.log(response.data.results);
       setPokemons(response.data.results);
     } catch (error) {
       console.log(error);
     }
   };
 
-  return <div>App</div>;
+  return (
+    <>
+      <article className="pt-6">
+        <header className="flex flex-col gap-2 w-full px-4 z-50">
+          Input form
+        </header>
+        <section className="pt-6 flex flex-col justify-center items-center overflow-auto z-0">
+          <div className="flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl">
+            {pokemons.length > 0 ? (
+              pokemons.map(({ url, name }, index) => <div> {name} </div>)
+            ) : (
+              <h2 className="font-medium text-lg text-slate-900 mb-1">
+                포켓몬이 없습니다.
+              </h2>
+            )}
+          </div>
+        </section>
+      </article>
+    </>
+  );
 };
 
 export default App;
