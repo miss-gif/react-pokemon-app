66b72fa2c7a9725b5accab979ec3060e0ccc99e5
miss-gif <ddc.akari@gmail.com>
2024-10-18 01:43:10 +0900

    Refactor: Add Type component and update DetailPage layout

--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -4,6 +4,10 @@ import { Link, useParams } from "react-router-dom"; // useParams를 사용해 UR
 import { Loading } from "../../assets/Loading";
 import { LessThan } from "../../assets/LessThan";
 import { GreaterThan } from "./../../assets/GreaterThan";
+import { ArrowLeft } from "../../assets/ArrowLeft";
+import { Balance } from "./../../assets/Balance";
+import { Vector } from "./../../assets/Vector";
+import Type from "../../components/Type";
 
 const DetailPage = () => {
   const [pokemon, setPokemon] = useState();
@@ -145,6 +149,80 @@ const DetailPage = () => {
             <GreaterThan className="w-5 h-8 p-1" />
           </Link>
         )}
+
+        <section className="w-full flex flex-col z-20 items-center justify-end relative h-full">
+          <div className="absolute z-30 top-6 flex items-center w-full justify-between px-2">
+            <div className="flex items-center gap-1">
+              <Link to="/">
+                <ArrowLeft className="w-6 h-8 text-zinc-200" />
+              </Link>
+              <h2 className="text-zinc-200 font-bold  text-xl capitalize ">
+                {pokemon.name}
+              </h2>
+            </div>
+            <div className="text-zinc-200 font-bold text-md">
+              #{pokemon.id.toString().padStart(3, "00")}
+            </div>
+          </div>
+
+          <div className="relative h-auto max-w-[15.5rem] z-20 mt-6 -mb-16">
+            <img
+              src={img}
+              width="100%"
+              height="auto"
+              loading="lazy"
+              alt={pokemon.name}
+              className={`object-contain h-full`}
+            />
+          </div>
+        </section>
+
+        <section className="w-full min-h-[65%] h-full bg-gray-800 z-10 pt-14 flex flex-col items-center gap-3  px-5 pb-4">
+          <div className="flex items-center justify-center gap-4">
+            {/* {포켓몬 타입} */}
+            {pokemon.types.map((type) => (
+              <Type key={type} type={type} />
+            ))}
+          </div>
+          <h2 className={`text-base font-semibold ${text}`}>정보</h2>
+          <div className="flex  w-full items-center justify-between max-w-[400px] text-center">
+            <div className="w-full">
+              <h4 className="text-[0.5rem] text-zinc-100">Weight</h4>
+              <div className="text-sm flex mt-1 gap-2 justify-center text-zinc-200">
+                <Balance />
+                {pokemon.weight}kg
+              </div>
+            </div>
+            <div className="w-full">
+              <h4 className="text-[0.5rem] text-zinc-100">Weight</h4>
+              <div className="text-sm flex mt-1 gap-2 justify-center text-zinc-200">
+                <Vector />
+                {pokemon.height}m
+              </div>
+            </div>
+            <div className="w-full">
+              <h4 className="text-[0.5rem] text-zinc-100">ability</h4>
+              {pokemon.abilities.map((ability) => (
+                <div
+                  key={ability}
+                  className="text-zinc-100 capitalize text-[0.5rem]"
+                >
+                  {ability}
+                </div>
+              ))}
+            </div>
+          </div>
+          <h2 className={`text-base font-semibold ${text}`}>기본 능력치</h2>
+          <div className="w-full text-zinc-100">Stat</div>
+          {pokemon.DamageRelations && (
+            <div className="w-10/12">
+              <h2 className={`text-base font-semibold ${text} text-center`}>
+                데미지 관계
+              </h2>
+              데미지
+            </div>
+          )}
+        </section>
       </div>
     </article>
   ); // DetailPage 컴포넌트를 렌더링합니다.
