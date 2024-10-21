4865482fa77fce2f13835380abf3f55a9c97c98f
miss-gif <ddc.akari@gmail.com>
2024-10-22 02:31:46 +0900

    Refactor: Add NavBar component and update App layout

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvUtwGwdUODEegWvRvtWjBN7MVN2X_axY",
+  authDomain: "react-pokemon-app-abf15.firebaseapp.com",
+  projectId: "react-pokemon-app-abf15",
+  storageBucket: "react-pokemon-app-abf15.appspot.com",
+  messagingSenderId: "267905848349",
+  appId: "1:267905848349:web:422bbdc982863fd81ea2f4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
