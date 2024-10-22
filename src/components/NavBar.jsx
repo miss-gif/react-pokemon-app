6591fc0983b7323a68d45fa8258a3c372cdd0c33
miss-gif <ddc.akari@gmail.com>
2024-10-23 07:42:28 +0900

    Refactor: Update NavBar component to include Google authentication on the login page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,28 @@
 import classNames from "classnames";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import app from "./../../firebase";
 
 const NavBar = () => {
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
   const [show, setShow] = useState(false);
-  console.log(show);
+
+  const { pathname } = useLocation();
 
   const img = `http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/152.png`;
+
+  const handleAuth = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   const listener = () => {
     if (window.scrollY > 10) {
@@ -28,7 +44,7 @@ const NavBar = () => {
       className={classNames(
         "fixed top-0 left-0 right-0 h-[70px] flex justify-between items-center px-[36px] z-[100] ",
         {
-          "bg-blue-200": show,
+          "bg-slate-200": show,
           "bg-transparent": !show,
         }
       )}
@@ -36,9 +52,17 @@ const NavBar = () => {
       <a className="p-0 w-[50px] mt-4" href="/">
         <img className="w-full" src={img} alt="poke logo" />
       </a>
-      <Link className="tracking-[2px]" to={"/login"}>
-        Login
-      </Link>
+      {pathname === "/login" ? (
+        <Link
+          className="tracking-[2px] px-[12px] py-[16px] bg-slate-400 uppercase text-white text-sm rounded transition-all duration-300 ease-in-out hover:bg-slate-500"
+          to={"/login"}
+          onClick={handleAuth}
+        >
+          Login
+        </Link>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
