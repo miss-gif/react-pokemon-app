c335b8462cb82978b80a230de767eff4f5331d5c
miss-gif <ddc.akari@gmail.com>
2024-10-25 23:20:38 +0900

    Refactor: Remove unused import in ArrowLeft component

--- src/assets/ArrowLeft.tsx
+++ src/assets/ArrowLeft.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { ClassNameProps } from "../types/ClassNameProps";
 
 export const ArrowLeft = ({ className: CN = "" }: ClassNameProps) => (
--- src/assets/Vector.tsx
+++ src/assets/Vector.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const Vector = () => (
   <svg
     width="8"
